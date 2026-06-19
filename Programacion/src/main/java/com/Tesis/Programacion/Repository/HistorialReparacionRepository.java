8cecba02f7164308ff8fb38fbe976dd8c4d36367
Leandro <leandrodanteciriaco@gmail.com>
2026-06-19 12:23:02 -0300

    se modifico las reparaciones y como se piden

--- Programacion/src/main/java/com/Tesis/Programacion/Repository/HistorialReparacionRepository.java
+++ Programacion/src/main/java/com/Tesis/Programacion/Repository/HistorialReparacionRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HistorialReparacionRepository extends JpaRepository<HistorialReparacion,Long> {
-    @Query("SELECT COUNT(h) from HistorialReparacion h WHERE h.id=:tallerId AND h.estadoReparacion!='ENTREGADO'")
-    Integer contarReparacionesActivas(@Param("tallerId")Long tallerId);
+    @Query("SELECT COUNT(h) from HistorialReparacion h WHERE h.taller.id = :tallerId AND h.estadoReparacion != 'ENTREGADO'")
+    Integer contarReparacionesActivas(@Param("tallerId") Long tallerId);
+
+    List<HistorialReparacion> findByTallerId(Long tallerId);
 }
