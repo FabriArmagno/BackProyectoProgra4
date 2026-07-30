9aca3e636c97478ea61c29c2bff0e0455eadc7d0
Bigfa12 <milrayas7@gmail.com>
2026-07-30 00:16:51 -0300

    Ver mi Perfil

--- Programacion/src/main/java/com/Tesis/Programacion/Controller/UsuarioController.java
+++ Programacion/src/main/java/com/Tesis/Programacion/Controller/UsuarioController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@PreAuthorize("hasRole('ADMIN')")
+@PreAuthorize("hasRole('ADMIN') or hasRole('EMPLEADO')")
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
